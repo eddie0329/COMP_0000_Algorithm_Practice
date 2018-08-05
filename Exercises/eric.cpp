2816ab151125f438975d552de69b52770ca1821c
eddie0329 <fdsa0329@naver.com>
2018-08-05 04:01:40 -0700

    sedf

--- Exercises/eric.cpp
+++ Exercises/eric.cpp
@@ -4,11 +4,18 @@
 using namespace std;
 
 
-//moving elements to second_room
-void move(vector <string> first_room, vector <string> second_room, string a) {
-  for(int i = 0; i < first_room.size(); i++) {
-    if(first_room[i] == a) {
-      second_room.push_back(first_room[i]);
+//moving elements, located in first_room, to second_room
+void move(vector <string> &origin_room, vector <string> &dest_room, string element_to_move_1 = "", string element_to_move_2 = "") {
+  for(int i = 0; i < origin_room.size(); i++) {
+    if(origin_room[i] == element_to_move_1 || origin_room[i] == element_to_move_2) {
+      dest_room.push_back(origin_room[i]);
+    }
+  }
+  for(int i = 0; i < origin_room.size(); i++) {
+    for(int j =0; j < dest_room.size(); j++) {
+      if(origin_room[i] == dest_room[j]) {
+        origin_room.erase(origin_room.begin() + i);
+      }
     }
   }
 }
@@ -18,17 +25,60 @@ void move(vector <string> first_room, vector <string> second_room, string a) {
 int main() {
   vector <string> first_room;
   vector <string> second_room;
-  string a = "Eric";
 
   first_room.push_back("Eric");
   first_room.push_back("Snake");
   first_room.push_back("Rabbit");
   first_room.push_back("Bread");
 
-  cout<<first_room[0]<<endl;
+  while(1) {
+    //braek the infinite loop
+    if(second_room.size() == 4) {
+      break;
+    }
+    //start the loop   Snake, Bread --- Eric, Rabbit
+    else if(first_room.size() == 4) {
+      move(first_room, second_room, "Eric", "Rabbit");
+      continue;
+    }
+    else {
+      for(int i = 0; i < first_room.size(); i ++) {
+        // Snake, Bread --- Eric, Rabbit     ->     Eric, Snake, Bread --- Rabbit
+        if(first_room[i] != "Eric" && first_room[i] != "Rabbit" && first_room[i] == "Snake" && first_room[i] == "Bread") {
+          move(second_room, first_room, "Eric");
+          continue;
+        }
 
-  move(first_room, second_room, a);
+        //Eric, Snake, Bread --- Rabbit        -> Bread --- Eric, Snake, Rabbit
+        else if(first_room[i] == "Eric" && first_room[i] != "Rabbit" && first_room[i] == "Snake" && first_room[i] == "Bread") {
+          move(first_room, second_room, "Eric", "Snake");
+          continue;
+        }
 
-  cout<<second_room[0]<<endl;
+        //Bread --- Eric, Snake, Rabbit         -> Eric, Rabbit, Bread --- Snake
+        else if(first_room[i] != "Eric" && first_room[i] != "Rabbit" && first_room[i] != "Snake" && first_room[i] == "Bread") {
+          move(second_room, first_room, "Eric", "Rabbit");
+          continue;
+        }
 
+        //Eric, Rabbit, Bread --- Snake          -> Rabbit --- Eric, Bread, Snake
+        else if(first_room[i] == "Eric" && first_room[i] == "Rabbit" && first_room[i] != "Snake" && first_room[i] == "Bread") {
+          move(first_room, second_room, "Eric", "Bread");
+          continue;
+        }
+
+        //Rabbit --- Eric, Bread, Snake           -> Rabbit, Eric --- Bread, Snake
+        else if(first_room[i] != "Eric" && first_room[i] == "Rabbit" && first_room[i] != "Snake" && first_room[i] != "Bread") {
+          move(first_room, second_room, "Eric");
+          continue;
+        }
+
+        //Rabbit, Eric --- Bread, Snake            -> ---Eric, Rabbit, Bread, Snake
+        else if(first_room[i] == "Eric" && first_room[i] == "Rabbit" && first_room[i] != "Snake" && first_room[i] != "Bread") {
+          move(first_room, second_room, "Eric", "Rabbit");
+          continue;
+        }
+      }
+    }
+  }
 }
